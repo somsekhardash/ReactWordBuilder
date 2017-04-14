bfe1b8aac8ee4d440c9a54b00147f09246429371
Somsekhar Dash <sdash7@sapient.com>
2017-04-14 16:28:19 +0530

    done

--- src/app/components/cardList.js
+++ src/app/components/cardList.js
@@ -12,35 +12,46 @@ class cardList extends React.Component{
 			this.props.itemDeleted(this.props.words.data);
 		}
 		
+		showThis(event){
+			if(event.target.parentNode.parentNode.classList.contains("active"))
+				event.target.parentNode.parentNode.classList.remove("active");
+			else 
+				event.target.parentNode.parentNode.classList.add("active");
+				
+			if(event.target.parentNode.nextElementSibling.classList.contains("hide"))
+				event.target.parentNode.nextElementSibling.classList.remove("hide");
+			else
+				event.target.parentNode.nextElementSibling.classList.add("hide");	
+		}		
+		
 	    render(){
 		let content= <div></div>;
-		if(!!this.props.words.data){
-			console.log(this.props.words.data);
+		if(!!this.props.words.data){	
             content = this.props.words.data.map((item,i) => 
-				<div key={i} className="col-lg-6">
-					<div className="ui card margin-bottom-25">
+				<div key={i} className="full-width">
+					<div className="ui card full-width">
 						<div className="content">
-							<div className="header">{item.word}</div>
+							<div className="header col-lg-8 no-padding" onClick={this.showThis}>{item.word}</div>
+							<button className="btn btn-danger btn-xs orange" data-item={i} onClick={this.deleteIt.bind(this)}>Delete</button>
+							<button className="btn btn-primary btn-xs" data-item={i} onClick={this.UpdateIt.bind(this)}>Update</button>
 						</div>
-						<div className="content">
-							<h4 className="ui sub header blue">{item.meaning}</h4>
-							<div className="ui small feed">
-							<div className="event">
-								<div className="content">
-								<div className="summary">
-									{item.example}
+						<div className="content no-up-border hide">
+							<div className="full-width col-lg-12 no-padding">								
+								<h4 className="ui sub header blue">{item.meaning}</h4>
+								<div className="ui small feed">
+									<div className="event">
+										<div className="summary">
+											{item.example}
+										</div>
+									</div>
 								</div>
-								</div>
-							</div>
 							</div>
 							<ul className="parent no-padding margin-bottom-0">
 								{item.synonyms.map((word,j) => <li key={j} className="ui image label">
 								{word}</li>)}
 							</ul>
-						</div>
-						<div className="extra content">
-							<button className="ui button orange" data-item={i} onClick={this.deleteIt.bind(this)}>Delete</button>
-							<button className="ui button teal" data-item={i} onClick={this.UpdateIt.bind(this)}>Update</button>
+							
+							
 						</div>
 					</div>
 				</div>
